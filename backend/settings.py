8172acbf8a6512bca1ae09a95a20d334d3dc312f
lenoopz <lenoopz@gmail.com>
2023-01-19 08:09:05 +0530

    site update

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!s52@!vqb=7#6d@4@jr&pl&k+sfp=g1e_2**vme^rmp!#ofncq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.onrender.com']
 
